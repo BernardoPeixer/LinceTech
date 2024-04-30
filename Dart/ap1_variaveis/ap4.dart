feb6c44ee764c872fccb480feb9c10a23a9b0be4
BernardoPeixer <bernardo.peixer.stahelin@gmail.com>
2024-04-30 15:37:32 -0300

    ap1_variaveis ap4.dart

--- Dart/ap1_variaveis/ap4.dart
+++ Dart/ap1_variaveis/ap4.dart
@@ -1,3 +1,26 @@
+import 'dart:math';
+
 void main() {
-  
+  // Criando Instância Random
+  Random random = Random();
+
+  // Definindo Variáveis 
+  int numero1 = random.nextInt(100) + 1;
+  int numero2 = random.nextInt(100) + 1;
+
+  // Divisão e Variável Resultado
+  double resultado = (numero1 / numero2);
+
+  // Salvando Parte Inteira
+  int parteInteira = resultado.toInt();
+
+  // Salvando Parte Decimal
+  double parteDecimal = resultado - parteInteira;
+
+  // Imprimindo Variáveis
+  print("Primeira variável: $numero1");
+  print("Segunda variável: $numero2");
+  print("Resultado da divisão: $resultado");
+  print("Parte inteira do resultado: $parteInteira");
+  print("Parte decimal do resultado: $parteDecimal");
 }
