af172934d2c64a2ee0e9702dad96920d322ab85d
BernardoPeixer <bernardo.peixer.stahelin@gmail.com>
2024-04-30 15:36:49 -0300

    ap1_variaveis ap2.dart

--- Dart/ap1_variaveis/ap2.dart
+++ Dart/ap1_variaveis/ap2.dart
@@ -1,3 +1,13 @@
 void main() {
-  
+  // Definindo Variáveis
+  const double nota1 = 7.0;
+  const double nota2 = 8.0;
+  const double nota3 = 5.3;
+  const double nota4 = 9.7;
+
+  // Calculo da Média
+  final double mediaFinal = (nota1 + nota2 + nota3 + nota4)/4;
+
+  // Imprimindo
+  print("A média entre [ $nota1 , $nota2 , $nota3 , $nota4 ] = $mediaFinal");
 }
