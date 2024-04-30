523e048858529431890246a158a1f55ed58cc811
BernardoPeixer <bernardo.peixer.stahelin@gmail.com>
2024-04-30 15:37:11 -0300

    ap1_variaveis ap3.dart

--- Dart/ap1_variaveis/ap3.dart
+++ Dart/ap1_variaveis/ap3.dart
@@ -1,3 +1,23 @@
+import 'dart:math';
+
 void main() {
-  
+  // Criando Instância Random
+  Random random = Random();
+
+  // Declarando Variáveis Aleatórias
+  final int numero1 = random.nextInt(101);
+  final int numero2 = random.nextInt(101);
+
+  // Imprimindo Valores Originais
+  print("O valor original do número 1 é: $numero1");
+  print("O valor original do número 2 é: $numero2");
+
+  // Invertendo Valores
+  final int temp = numero1;
+  final int numero1Invertido = numero2;
+  final int numero2Invertido = temp;
+
+  // Imprimindo Valores Invertidos
+  print("O valor invertido do número 1 é: $numero1Invertido");
+  print("O valor invertido do número 2 é: $numero2Invertido");
 }
